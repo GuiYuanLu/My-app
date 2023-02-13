045844dd119d8f5334f13b8105d6994ca5f0cd0d
Ray <1194494392@qq.com>
2023-02-13 18:08:01 +0800

    test chatGpt

--- src/components/demo8/Test.jsx
+++ src/components/demo8/Test.jsx
@@ -1,30 +1,39 @@
- function getImageUrl(person, size = "s") {
+function getImageUrl(person, size = "s") {
   return "https://i.imgur.com/" + person.imageId + size + ".jpg";
 }
 
-function Avatar(person,size){
-    return(
-        <div>
-            <img src={getImageUrl(person)} alt={person.name} width={size} height={size} />
-        </div>
-    )
+function Avatar(person, size) {
+  return (
+    <div>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+        width={size}
+        height={size}
+      />
+    </div>
+  );
 }
 
-function Card({children}){
-    return(
-        <>
-            {children}
-        </>
-    )
+function Card({ children }) {
+  return <>{children}</>;
 }
 
+export function Clock({ time }) {
+  let hours = time.getHours();
+  let className = time.getHours() <= 6 ? 'night' : 'day';
+  return <h1 className={className}>{time.toLocaleTimeString()}</h1>;
+}
 
-export default function Home(){
-    return(
-        <div>
-            <Card>
-                <Avatar size={100} person={{imageId: 'YfeOqp2',name:"Katsuko Saruhashi"}}/>
-            </Card>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <Card>
+        <Avatar
+          size={100}
+          person={{ imageId: "YfeOqp2", name: "Katsuko Saruhashi" }}
+        />
+      </Card>
+    </div>
+  );
 }
