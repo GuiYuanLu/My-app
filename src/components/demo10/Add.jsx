ac9148118820026fd885ea0a2fc76f08d3e314d1
Ray <1194494392@qq.com>
2023-02-15 00:08:53 +0800

    明天写一版分散开的

--- src/components/demo10/Add.jsx
+++ src/components/demo10/Add.jsx
@@ -48,6 +48,50 @@ export function ThreeAdd() {
     </>
   );
 }
+//[e.target.name]: e.target.value, <input name="email" value={person.email} onChange={handleChange} />
+//动态属性匹配
+export default function Form() {
+  const [person, setPerson] = useState({
+    firstName: "Barbara",
+    lastName: "Hepworth",
+    email: "bhepworth@sculpture.com",
+  });
+
+  function handleChange(e) {
+    setPerson({
+      ...person,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  return (
+    <>
+      <label>
+        First name:
+        <input
+          name="firstName"
+          value={person.firstName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          name="lastName"
+          value={person.lastName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input name="email" value={person.email} onChange={handleChange} />
+      </label>
+      <p>
+        {person.firstName} {person.lastName} ({person.email})
+      </p>
+    </>
+  );
+}
 //如果是这种情况就结合上面两者情况 第一句setNumber React拿到值为5 然后运算完给他赋值 所以结果为6
 export function SixAdd() {
   const [number, setNumber] = useState(0);
@@ -59,6 +103,9 @@ export function SixAdd() {
         这种情况就结合上面两者情况 第一句setNumber React拿到值为5
         然后运算完给他赋值 所以结果为6
       </h2>
+      <h3>
+        这确保当您递增或递减计数器时，您是根据其最新状态而不是单击时的状态进行操作这就是更新函数的意义
+      </h3>
       <button
         onClick={() => {
           setNumber(number + 5);
@@ -70,3 +117,4 @@ export function SixAdd() {
     </>
   );
 }
+//
