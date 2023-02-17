091a507cdf70f662db1521dc201e43e70a1a273b
Ray <1194494392@qq.com>
2023-02-17 16:21:31 +0800

    更改router 位置

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-
-import Home from "./components/Home";
-import About from "./components/About";
-import Posts from "./components/Posts";
-import PostLists from "./components/PostLists";
-import Post from "./components/Post";
+import { BrowserRouter } from "react-router-dom";
+import Mrouter from "./router/index.jsx";
 
 function App() {
   return (
-    <Router>
-      <nav style={{ margin: 10 }}>
-        <Link to="/" style={{ padding: 5 }}>
-          Home
-        </Link>
-        <Link to="/about" style={{ padding: 5 }}>
-          About
-        </Link>
-        <Link to="/posts" style={{ padding: 5 }}>
-          Posts
-        </Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="posts" element={<Posts />}>
-          <Route index element={<PostLists />} />
-          <Route path=":slug" element={<Post />} />
-        </Route>
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <Mrouter />
+    </BrowserRouter>
   );
 }
 
