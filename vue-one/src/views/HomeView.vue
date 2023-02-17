c598c70ca40765bdd9cee014a4a6f519fc26b067
Yaney Liu <yf_liu@apple.com>
2023-02-17 16:50:07 +0800

    feat: 首页

--- vue-one/src/views/HomeView.vue
+++ vue-one/src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld :msg="msg"></HelloWorld>
+    <NavView></NavView>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import NavView from "../views/NavView.vue";
 export default defineComponent({
   name: "HomeView",
   components: {
     HelloWorld,
+    NavView,
+  },
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js + TypeScript App",
+    };
   },
 });
 </script>
---
+++ vue-one/src/views/NavView.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <div v-for="(index, item) in NavOptions" :key="index">
+      {{ item.name }}
+    </div>
+  </section>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "NavView",
+  components: {},
+  data() {
+    return {
+      NavOptions: [
+        { path: "/", name: "首页" },
+        { path: "/", name: "介绍" },
+        { path: "/", name: "文化" },
+        { path: "/", name: "反馈" },
+      ],
+    };
+  },
+});
+</script>
