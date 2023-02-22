09f898ece3311fa3bc752528025372dfcc73fe0e
Ray <1194494392@qq.com>
2023-02-22 15:32:38 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { useEffect } from "react";
-import { Routes, Route, useNavigate, NavLink } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "../views/Home/Home";
 import Login from "../views/Login/Login";
+import About from "../views/About/About";
 
 export default function RouterIndex() {
 	return (
-		<div>
+		<>
 			<Routes>
 				<Route path="/" index element={<Login />} />
 				<Route path="/home" element={<Home />} />
 			</Routes>
-		</div>
+
+			<Link to="/about"></Link>
+			<h1>About</h1>
+			<Link to="/home"></Link>
+			<h1>Home</h1>
+		</>
 	);
 }
