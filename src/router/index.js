891bb7ff2fad84d40f2e507418c59695fad271a3
Ray <1194494392@qq.com>
2023-02-22 15:01:53 +0800

    no word

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,16 @@
 import React from "react";
 import { useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, NavLink } from "react-router-dom";
 import Home from "../views/Home/Home";
 import Login from "../views/Login/Login";
 
 export default function RouterIndex() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/"
-          render={() =>
-            localStorage.getItem("token") ? (
-              <Home></Home>
-            ) : (
-              <Redirect to="/login" />
-            )
-          }
-        />
-      </Routes>
-    </div>
-  );
-}
-
-function Redirect(to) {
-  const navigate = useNavigate();
-  useEffect(() => {
-    navigate(to, { replace: true });
-  }, []);
-  return null;
+	return (
+		<div>
+			<Routes>
+				<Route path="/" index element={<Login />} />
+				<Route path="/home" element={<Home />} />
+			</Routes>
+		</div>
+	);
 }
--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 export default function Login() {
-  const navigate = useNavigate();
-  console.log(navigate);
-  return (
-    <form action="">
-      <label htmlFor="">
-        账号:
-        <input type="text" name="number" />
-      </label>
-      <label htmlFor="" id="pwd">
-        密码:
-        <input type="password" name="pwd" id="pwd" />
-      </label>
-      <button
-        onClick={() => {
-          localStorage.setItem("token", "123");
-        }}
-      >
-        登陆
-      </button>
-    </form>
-  );
+	const navigate = useNavigate();
+	return (
+		<form action="">
+			<label htmlFor="">
+				账号:
+				<input type="text" name="number" />
+			</label>
+			<label htmlFor="" id="pwd">
+				密码:
+				<input type="password" name="pwd" id="pwd" />
+			</label>
+			<button
+				onClick={() => {
+					localStorage.setItem("token", "123");
+				}}
+			>
+				登陆
+			</button>
+		</form>
+	);
 }
